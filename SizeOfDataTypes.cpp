d5b9f166237fbd8c305028cc654b11283a67ee81
mrityunjay-007 <mrityunjaynishad044@gmail.com>
2025-04-17 20:29:42 +0530

    Add files via upload

---
+++ SizeOfDataTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Size of Fundamental data types are: "<<endl;
+    cout<<" The Size of Char is: "<<sizeof(char)<<" Bytes"<<endl;
+    cout<<" The Size of short is: "<<sizeof(short)<<" Bytes"<<endl;
+    cout<<" The Size of int is: "<<sizeof(int)<<" Bytes"<<endl;
+    cout<<" The Size of long is: "<<sizeof(long)<<" Bytes"<<endl;
+    cout<<" The Size of long long is: "<<sizeof(long long)<<" Bytes"<<endl;
+    cout<<" The Size of float is: "<<sizeof(float)<<" Bytes"<<endl;
+    cout<<" The Size of double is: "<<sizeof(double)<<" Bytes"<<endl;
+    cout<<" The Size of long double is: "<<sizeof(long double)<<" Bytes"<<endl;
+    cout<<" The Size of bool is: "<<sizeof(bool)<<" Bytes"<<endl;
+
+}
