d811d5fddfc57ef075419aea012ca8262696c2b7
mrityunjay-007 <mrityunjaynishad044@gmail.com>
2025-04-17 20:19:59 +0530

    solved assignment files are uploaded

---
+++ Cube.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the integer: "<<endl;
+    cin>>a;
+
+    int cube = a*a*a;
+    cout<<"Cube of the integer is: "<< cube<<endl;
+}
