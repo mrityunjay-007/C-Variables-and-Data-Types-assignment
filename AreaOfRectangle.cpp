45c01286d6efeffbbddd2aa2a0095a7d2f16bdc6
mrityunjay-007 <mrityunjaynishad044@gmail.com>
2025-04-17 20:15:38 +0530

    solved assignmet files are added

---
+++ AreaOfRectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length, breadth;
+    cout<<"Enter the Length of Rectangle: "<<endl;
+    cin>>length;
+    cout<<"Enter the Breadth of Rectangle: "<<endl;
+    cin>>breadth;
+     int area = length * breadth;
+
+     cout<<"Area of Rectangle is: "<<area<<endl;
+     return 0;
+}
